5b1e090e48d1ad7c80a9b4365434e3bbc71c16ee
Artur Chmielewski <arturchmiel96@gmail.com>
2021-12-05 00:10:27 +0100

    Created popup comfirm and added him to dashboard interface windows

--- src/components/organisms/DyspoSendBlockedWindow/DyspoSendBlockedWindow.js
+++ src/components/organisms/DyspoSendBlockedWindow/DyspoSendBlockedWindow.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PopupComfirm from 'components/molecues/PopupComfirm/PopupComfirm';
 import { InterfaceWindowTemplate } from 'components/templates/InterfaceWindowTemplate/InterfaceWindowTemplate';
 import { InterfaceWindowTitle } from 'components/atoms/InterfaceWindowTitle/InterfaceWindowTitle';
 import { SubmitButton } from 'components/atoms/SubmitButton/SubmitButton';
 import { StyledSubTitle } from './DyspoSendBlockedWindow.style';
 
+const TITLEPOPUP = 'Czy napewno chcesz zakończy boecny okres dla grafiku?';
+
 const DyspoSendBlockedWindow = () => {
+  const [isVisible, setVisible] = useState(false);
+
+  const toggleVisible = () => {
+    setVisible(!isVisible);
+  };
   return (
-    <InterfaceWindowTemplate>
-      <InterfaceWindowTitle>Wysyłanie dyspozycji zablokowane !</InterfaceWindowTitle>
-      <StyledSubTitle>
-        Wysyłanie dyspozycji zostało zablokowane, dodaj szablon grafiku aby móc wygenerować grafik
-      </StyledSubTitle>
-      <SubmitButton isDangerous>Zakończ okres</SubmitButton>
-    </InterfaceWindowTemplate>
+    <>
+      <PopupComfirm
+        title={TITLEPOPUP}
+        isVisible={isVisible}
+        handleComfirm={toggleVisible}
+        handleCancel={toggleVisible}
+      />
+      <InterfaceWindowTemplate>
+        <InterfaceWindowTitle>Wysyłanie dyspozycji zablokowane !</InterfaceWindowTitle>
+        <StyledSubTitle>
+          Wysyłanie dyspozycji zostało zablokowane, dodaj szablon grafiku aby móc wygenerować grafik
+        </StyledSubTitle>
+        <SubmitButton isDangerous onClick={toggleVisible}>
+          Zakończ okres
+        </SubmitButton>
+      </InterfaceWindowTemplate>
+    </>
   );
 };
 
