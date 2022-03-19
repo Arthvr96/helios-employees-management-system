cf88db3748b270a1d4bc8fe6d00729b673cb4369
Artur Chmielewski <arturchmiel96@gmail.com>
2022-03-19 16:52:39 +0100

    bugfix 68 complete

--- src/components/views/AdminViews/DashboardView/DashboardView.js
+++ src/components/views/AdminViews/DashboardView/DashboardView.js
@@ -4,12 +4,14 @@ import NewCycleWindow from 'components/organisms/NewCycleWindow/NewCycleWindow';
 import ActiveCycleWindows from 'components/organisms/ActiveCycleWindows/ActiveCycleWindows';
 import DispoSendBlockedWindow from 'components/organisms/DispoSendBlockedWindow/DispoSendBlockedWindow';
 import { useGlobalState } from 'providers/GlobalStateProvider/GlobalStateProvider';
+import LoadingScreen from 'components/molecules/LoadingScreen/LoadingScreen';
 
 const DashBoard = () => {
-  const { appState } = useGlobalState();
+  const { appState, isProcessingState } = useGlobalState();
 
   return (
     <ViewTemplate>
+      <LoadingScreen isVisible={isProcessingState} />
       {appState.state === 'nonActive' ? <NewCycleWindow /> : null}
       {appState.state === 'active' ? <ActiveCycleWindows /> : null}
       {appState.state === 'blocked' ? <DispoSendBlockedWindow /> : null}
