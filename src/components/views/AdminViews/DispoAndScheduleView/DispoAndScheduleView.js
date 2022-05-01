ed977530c8f3bce49ca26cefec8ed6a1e3db01d1
Artur Chmielewski <arturchmiel96@gmail.com>
2022-05-01 23:14:20 +0200

    fetch list users for dev use

--- src/components/views/AdminViews/DispoAndScheduleView/DispoAndScheduleView.js
+++ src/components/views/AdminViews/DispoAndScheduleView/DispoAndScheduleView.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ViewTemplate from 'components/templates/ViewTemplate/ViewTemplate';
 import MenuSelectVisiblePanel from 'components/molecules/EmployeesManagementMenu/MenuSelectVisiblePanel';
 import DispositionsArchive from 'components/organisms/DispositionsArchive/DispositionsArchive';
+import heliosAppSdk from 'HeliosAppSdk/HeliosAppSdk';
 
 const links = [
   ['1', 'Wysłane dyspozycje'],
@@ -10,6 +11,18 @@ const links = [
 
 const DispoAndScheduleView = () => {
   const [page, setPage] = useState('0');
+  const { __handleGetDocs__ } = heliosAppSdk.__firestoreFunctionsPrivate__;
+  const { users } = heliosAppSdk.firestoreConstants.paths;
+  useEffect(() => {
+    __handleGetDocs__(users, 'role', '==', 'user').then((r) => {
+      const arr = [];
+      r.forEach((el) => {
+        arr.push(el.data());
+      });
+      console.log(arr);
+      console.log(JSON.stringify(arr));
+    });
+  }, []);
 
   const handleSetPage = (nrPage) => {
     if (page === nrPage) {
